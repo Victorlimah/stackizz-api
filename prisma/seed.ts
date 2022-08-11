6e801f5fb0c6f9c2f8feb199ff38b92b61f3c847
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-08-11 10:44:13 -0300

    feat: add questions in seed for prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { CreateAnswer } from './../src/interfaces/createData';
+import { CreateAnswer } from "./../src/interfaces/createData";
 import { PrismaClient } from "@prisma/client";
 import {
   CreateUser,
@@ -62,7 +62,7 @@ async function main() {
     },
   ];
 
-  const createQuestionsT1M1:  CreateQuestion[] = [
+  const createQuestionsT1M1: CreateQuestion[] = [
     {
       name: "Qual o nome do elemento HTML que representa um parágrafo?",
       topicId: 1,
@@ -71,9 +71,42 @@ async function main() {
       name: "Qual o nome do elemento HTML que representa um link?",
       topicId: 1,
     },
-  ]
+    {
+      name: "Como deve ser declarado um título da página?",
+      topicId: 1,
+    },
+    {
+      name: "Como devemos chamar um script css?",
+      topicId: 1,
+    },
+    {
+      name: "Qual a forma correta de se criar uma lista?",
+      topicId: 1,
+    },
+    {
+      name: "Qual tag abaixo pertence a um menu de navegação?",
+      topicId: 1,
+    },
+    {
+      name: "Qual atributo abaixo não pertence a tag <a>?",
+      topicId: 1,
+    },
+    {
+      name: "Qual a forma correta de se criar uma imagem?",
+      topicId: 1,
+    },
+    {
+      name: "Qual tag listada abaixo não é suportada no HTML 5?",
+      topicId: 1,
+    },
+    {
+      name: "Qual atributo devemos utilizar para inserir propriedades CSS?",
+      topicId: 1,
+    },
+  ];
 
   const CreateAnswersT1M1: CreateAnswer[] = [
+    // Question 1
     {
       text: "p",
       correct: true,
@@ -94,6 +127,7 @@ async function main() {
       correct: false,
       questionId: 1,
     },
+    // Question 2
     {
       text: "h1",
       correct: false,
@@ -113,7 +147,187 @@ async function main() {
       text: "img",
       correct: false,
       questionId: 2,
-    }
+    },
+    // Question 3
+    {
+      text: `<title title="titulo">`,
+      correct: false,
+      questionId: 3,
+    },
+    {
+      text: `<html title="Título aqui"></html>>`,
+      correct: false,
+      questionId: 3,
+    },
+    {
+      text: `<title>Título aqui</title>`,
+      correct: true,
+      questionId: 3,
+    },
+    {
+      text: `<title src="Título aqui" />`,
+      correct: false,
+      questionId: 3,
+    },
+    // Question 4
+    {
+      text: `<a href="link css"></a>`,
+      correct: false,
+      questionId: 4,
+    },
+    {
+      text: `<link rel="stylesheet" href="link css>`,
+      correct: true,
+      questionId: 4,
+    },
+    {
+      text: `<script href="link css">`,
+      correct: false,
+      questionId: 4,
+    },
+    {
+      text: `<style href="link css"/>`,
+      correct: false,
+      questionId: 4,
+    },
+    // Question 5
+    {
+      text: `
+      <list>
+        <item></item>
+      </list>`,
+      correct: false,
+      questionId: 5,
+    },
+    {
+      text: `
+      <ul>
+        <li></li>
+      </ul>`,
+      correct: true,
+      questionId: 5,
+    },
+    {
+      text: `
+      <ol>
+        <item></item>
+      </ol>`,
+      correct: false,
+      questionId: 5,
+    },
+    {
+      text: `
+      <li>
+        <item></item>
+      </li>`,
+      correct: false,
+      questionId: 5,
+    },
+    // Question 6
+    {
+      text: "header",
+      correct: false,
+      questionId: 6,
+    },
+    {
+      text: "menu",
+      correct: false,
+      questionId: 6,
+    },
+    {
+      text: "nav",
+      correct: true,
+      questionId: 6,
+    },
+    {
+      text: "itens",
+      correct: false,
+      questionId: 6,
+    },
+    // Question 7
+    {
+      text: `target=""`,
+      correct: false,
+      questionId: 7,
+    },
+    {
+      text: `rel=""`,
+      correct: false,
+      questionId: 7,
+    },
+    {
+      text: `href=""`,
+      correct: false,
+      questionId: 7,
+    },
+    {
+      text: `src=""`,
+      correct: true,
+      questionId: 7,
+    },
+    // Question 8
+    {
+      text: `<img src="link imagem">`,
+      correct: true,
+      questionId: 8,
+    },
+    {
+      text: `<figure src="link imagem"></figure>`,
+      correct: false,
+      questionId: 8,
+    },
+    {
+      text: `<img>link imagem</img>`,
+      correct: false,
+      questionId: 8,
+    },
+    {
+      text: `<image src="imagem.jpg" />`,
+      correct: false,
+      questionId: 8,
+    },
+    // Question 9
+    {
+      text: `<table>`,
+      correct: false,
+      questionId: 9,
+    },
+    {
+      text: `<iframe>`,
+      correct: false,
+      questionId: 9,
+    },
+    {
+      text: `<font>`,
+      correct: false,
+      questionId: 9,
+    },
+    {
+      text: `<header>`,
+      correct: false,
+      questionId: 9,
+    },
+    // Question 10
+    {
+      text: `style=""`,
+      correct: true,
+      questionId: 10,
+    },
+    {
+      text: `css-style=""`,
+      correct: true,
+      questionId: 10,
+    },
+    {
+      text: `css=""`,
+      correct: true,
+      questionId: 10,
+    },
+    {
+      text: `styled-component=""`,
+      correct: true,
+      questionId: 10,
+    },
   ];
 
   await prisma.$queryRaw`TRUNCATE TABLE users, modules, topics RESTART IDENTITY CASCADE;`;
